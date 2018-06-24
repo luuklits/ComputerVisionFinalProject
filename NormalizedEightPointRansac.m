2d9eb481ed784d8b800070d87c5a60ab502948ed
Luuk <luuklits@hotmail.com>
2018-06-24 11:57:32 +0200

    something

--- NormalizedEightPointRansac.m
+++ NormalizedEightPointRansac.m
@@ -1,9 +1,11 @@
-function [Fbest, BestInliers] = NormalizedEightPointRansac(A, threshold)
+function [Fbest, BestInliers, x1inliers, y1inliers, x2inliers, y2inliers] = NormalizedEightPointRansac(A, matches,f1, f2, threshold)
     %normalization of the data
-    x1 =  A(:,3);
-    y1 =  A(:,6);
-    x2 =  A(:,7);
-    y2 =  A(:,8);
+    index1 = matches(1,:);
+    index2 = matches(2,:);
+    x1 =  f1(1,index1)
+    y1 =  f1(2,index1)
+    x2 =  f2(1,index2)
+    y2 =  f2(2,index2)
     [T1, phat1] = Normalization(x1, y1);
     [T2, phat2] = Normalization(x2, y2);
     sprintf("Normalized the Fundamental Matrix")
@@ -12,4 +14,12 @@ function [Fbest, BestInliers] = NormalizedEightPointRansac(A, threshold)
     [Fbest, BestInliers] = RANSAC(phat1, phat2, threshold);
     sprintf("Executed the Eight-Point Algorithm with Ransac on the normalized Data")
     sprintf("Got %i inliers from the best transformation", length(BestInliers))
+    
+    InliersIndices1 = matches(1,BestInliers);
+    InliersIndices2 = matches(2,BestInliers);
+    x1inliers = f1(1,InliersIndices1);
+    y1inliers = f1(2,InliersIndices1);
+    x2inliers = f2(1,InliersIndices2);
+    y2inliers = f2(2,InliersIndices2);
+    
 end
--- main.m
+++ main.m
@@ -3,11 +3,11 @@ clear all; clc
 run('~/Documents/MATLAB/vlfeat-0.9.21/toolbox/vl_setup')
 
 %load images into variables
-img1 = rgb2gray(imread(char('left.jpg')));
-img2 = rgb2gray(imread(char('right.jpg')));
+img1 = rgb2gray(imread(char('model_castle/8ADT8586.JPG')));
+img2 = rgb2gray(imread(char('model_castle/8ADT8587.JPG')));
 
 %set thresholdfactor for the cornerness for the harris corner point detector
-thresholdFactor = 0.02;
+thresholdFactor = 0.95;
 
 [f1,d1] = FeaturesAndDescriptors(img1, thresholdFactor);
 [f2,d2] = FeaturesAndDescriptors(img2, thresholdFactor);
@@ -20,7 +20,23 @@ sprintf("Got %i matches", length(matches))
 [A, BestMatches] = FundamentalMatrix(img1, img2, scores, matches, f1, f2, 20, false);
 
 %execute the normalized eight point algorithm with RANSAC on A
-[Fbest, BestInliers] = NormalizedEightPointRansac(A, 0.001);
+[Fbest, BestInliers, x1inliers, y1inliers, x2inliers, y2inliers] = NormalizedEightPointRansac(A, matches, f1, f2, 0.001);
+
+figure
+imshowpair(img1,img2,'montage');
+
+for i = 1:1:length(x1inliers)
+    x1 = x1inliers(i);
+    x2 = x2inliers(i) + size(img1,2);
+    y1 = y1inliers(i);
+    y2 = y2inliers(i);
+    hold on
+    line([x1,x2],[y1,y2]);
+    hold on
+    plot(x1, y1, 'x');
+    hold on
+    plot(x2, y2, 'x');
+end
 
 
 
